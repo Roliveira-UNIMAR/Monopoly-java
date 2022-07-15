ea0ba9757710cc4c0bea02bcd9448254ad0296f0
Richard Rivera <richirrr1234@gmail.com>
2022-07-15 16:08:32 -0400

    Creacion de la clase Archivos y archivo.txt

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package main;
+import models;
 import java.util.Scanner;
 
 /**
---
+++ src/models/Archivo.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+/**
+ *
+ * @author RICHARD RIVERA
+ */
+public class Archivo {
+    
+    
+    //Se leera el txt con los datos de las propiedades (Casas, hoteles, etc...)
+    public static String Propiedades(){
+        
+        
+        
+        
+     return ;    
+    }
+    
+    
+    
+    //Me
+    public static String ArcaComunal(){
+        
+        return ;
+    }
+
+    
+    
+    public static String Fortuna(){
+        
+       return , 
+    }         
+} 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
