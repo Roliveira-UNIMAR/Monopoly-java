554a9f3eccfec2b3902cf3a8cac145a3e40eba00
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-16 13:27:28 -0400

    Creación incompleta Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

---
+++ src/models/Carcel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Rodrigo Oliveira 29.655.609
+ */
+public class Carcel extends Casilla {
+
+    public void enviarACarcel(Jugador jugador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
