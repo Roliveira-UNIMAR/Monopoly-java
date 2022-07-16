3a06859e6d8e8cfba24f36c08db90e1733f0acac
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-16 18:43:25 -0400

    Actulizando Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Propiedad.java
+++ src/models/Propiedad.java
@@ -1,7 +1,14 @@
 package models;
 
+/**
+ *
+ * @author Rodrigo Oliveira - 29.655.609
+ * @author Richard Rivera - 28.570.753
+ * @author Shaenndry Carreño - 30.299.949
+ */
 public class Propiedad extends Casilla {
     private String propietario;
+    private int costo;
     private int rentas[];
     private int cantCasas;
     private int costoCasa;
@@ -11,6 +18,7 @@ public class Propiedad extends Casilla {
     public Propiedad() {
         super();
         this.propietario = null;
+        this.costo = 0;
         this.rentas = new int[6];
         this.cantCasas = 0;
         this.costoCasa = 0;
@@ -18,12 +26,13 @@ public class Propiedad extends Casilla {
         this.hipotecada = false;
     }
     
-    public Propiedad(int pos, String n ,int r, int r1, int r2, int r3, int r4, int r5, int cos, int hipo) {
+    public Propiedad(int pos, String n, int cos, int r, int r1, int r2, int r3, int r4, int r5, int cosC, int hipo) {
         super(pos, n);
         this.propietario = null;
+        this.costo = cos;
         this.rentas = new int[] {r, r1, r2, r3, r4, r5};
         this.cantCasas = 0;
-        this.costoCasa = cos;
+        this.costoCasa = cosC;
         this.costoHipoteca = hipo;
         this.hipotecada = false;
     }
@@ -34,6 +43,10 @@ public class Propiedad extends Casilla {
     
     public void setPropietario(String ficha) {
         this.propietario = ficha;
+    }
+
+    public int getCosto() {
+        return costo;
     }
     
     public void setHipotecada(boolean tf) {
@@ -68,12 +81,28 @@ public class Propiedad extends Casilla {
                 return this.rentas[0];
         }
     }
-
+    
     public int getCostoCasa() {
         return this.costoCasa;
     }
 
     public int getCostoHipoteca() {
         return this.costoHipoteca;
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+        string += "\n\t" + this.nombre + "\n\n";
+        string += "Alquiler: " + this.rentas[0] + "$" + "\n\n";
+        string += "Con 1 Casa: " + this.rentas[1] + "$" + "\n";
+        string += "Con 2 Casa: " + this.rentas[2] + "$" + "\n";
+        string += "Con 3 Casa: " + this.rentas[3] + "$" + "\n";
+        string += "Con 4 Casa: " + this.rentas[4] + "$" + "\n";
+        string += "Con Hotel: " + this.rentas[5] + "$" + "\n\n";
+        string += "Hipoteca: " + this.costoHipoteca + "$" + "\n\n";
+        string += "Las Casa cuestan: " + this.costoCasa + "$" + "\n";
+        string += "El Hotel cuesta: 4 casas + " + this.costoCasa + "$";
+        return string;
     }
 }
