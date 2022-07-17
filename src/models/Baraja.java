1d9c2425b892581db76050b273e7b0df511b7632
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-17 02:21:34 -0400

    Actulizado Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Baraja.java
+++ src/models/Baraja.java
@@ -1,29 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import models.cartas.Carta;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 /**
  *
  * @author Rodrigo Oliveira - 29.655.609
+ * @author Richard Rivera - 28.570.753
  */
 public class Baraja {
     private ArrayList<Carta> baraja = new ArrayList<>();
 
-    public Carta tomarCarta(){
+    public Carta tomarCarta(Jugador jugador){
         Carta carta;
         carta = baraja.remove(0);
-        ponerFinal(carta);
+        ponerAbajo(carta);
         return carta;
     }
 
-    public void ponerFinal(Carta carta){
+    public void ponerAbajo(Carta carta){
         baraja.add(carta);
     }
 
