723dea17c453d2be55664c0ec3bb36b13df7c48d
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-17 02:17:47 -0400

    Actulizado Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Impuesto.java
+++ src/models/Impuesto.java
@@ -8,20 +8,16 @@ package models;
  *
  * @author RICHARD RIVERA
  */
-public class Impuesto extends Casilla{
-    
-    private final int CantPagar = -200;
-    
-    public Impuesto(){
-        super(38 ,"IMPUESTO");    
-    }
-    
-    public int getCantpagar() {
-        return CantPagar;
+public class Impuesto extends Casilla {
+    private int impuesto;
+
+    public Impuesto(int pos, String n, int impuesto){
+        super(pos,n);
+        this.impuesto = impuesto;
     }
 
     @Override
-    public void hacer(Jugador actualJugador) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void hacer(Jugador jugador) {
+        jugador.setDinero(-this.impuesto);
     }
 }
