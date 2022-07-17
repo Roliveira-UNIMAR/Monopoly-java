aea7fa6c312865a1e93df42839bc309005501287
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-16 21:57:52 -0400

    Actulizado Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Carcel.java
+++ src/models/Carcel.java
@@ -1,18 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
  *
  * @author Rodrigo Oliveira 29.655.609
  */
-public class Carcel extends Casilla {
-
-    public void enviarACarcel(Jugador jugador) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public class Carcel {
     
+    public static void enviarACarcel(Jugador jugador) {
+        jugador.enCarcel = true;
+        Mostrar.msj("Ahora estás en la cárcel durante los próximos turnos");
+        jugador.mover(10);
+        Monopolio.finTurno(jugador);
+    }
+
+    public static boolean carcelTurno(Jugador actualJugador) {
+        actualJugador.turnosCarcel++;
+        Mostrar.msj("Turnos en carcel: " + actualJugador.turnosCarcel);
+        
+        int tirada = Dado.lanzar();
+        if (actualJugador.turnosCarcel <= 3) { 
+            if (Dado.isDoble()) {
+                actualJugador.enCarcel = false;
+                actualJugador.mover(tirada);
+            } else {
+                if (actualJugador.turnosCarcel < 3) {
+                    Mostrar.msj("Se cobraron 50$ y sales de la carcel");
+                    actualJugador.enCarcel = false;
+                    actualJugador.setDinero(-50);
+                    actualJugador.mover(tirada);
+                } else {
+                    Mostrar.msj("Sigues en la carcel"); 
+                }   
+            }
+        }
+        return actualJugador.enCarcel;
+    }
 }
