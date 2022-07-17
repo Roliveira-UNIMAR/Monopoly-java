79fecaa5c55cb1758e2c708be752e3d2bcc5f1b9
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-16 21:23:01 -0400

    Actulizando Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Tablero.java
+++ src/models/Tablero.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
@@ -10,51 +5,79 @@ package models;
  * @author Rodrigo Oliveira - 29.655.609
  */
 public class Tablero {
-    Casilla[] tablero = new Casilla[39];
+    public static Casilla[] tablero = new Casilla[40];
+    public static Baraja arcaComunal = new Baraja();
+    public static Baraja fortuna = new Baraja();
     
     public static void iniciar() {
         Propiedad[] propiedades = Archivo.getPropiedades();
         
-        // Nota falta crear varias clases para hacer que esto funcione
-        tablero[0] = new Salida();
+        Casilla salida = new Casilla(0, "SALIDA") {
+            @Override
+            public void hacer(Jugador actualJugador) {}
+        };
+        
+        Casilla deVisita = new Casilla(10, "DE VISITA NADA MÁS ") {
+            @Override
+            public void hacer(Jugador actualJugador) {}
+        };
+        
+        Casilla paradaLibre = new Casilla(20, "PARADA LIBRE") {
+            @Override
+            public void hacer(Jugador actualJugador) {}
+        };
+        
+        Casilla irCarcel = new Casilla(30, "VÁYASE A LA CÁRCEL") {
+            @Override
+            public void hacer(Jugador actualJugador) { 
+                        Carcel.enviarACarcel(actualJugador);
+            }
+        };
+        
+        tablero[0] = salida;
         tablero[1] = propiedades[0];
-        tablero[2] = new ArcaComunal(2);
+        tablero[2] = new TomarCarta(2, "ARCA COMUNAL", arcaComunal);
         tablero[3] = propiedades[1];
         tablero[4] = new Impuesto(4, "IMPUESTO SOBRE INGRESOS PÁGUESE", 200);
         tablero[5] = new Ferrocaril(5, "FERROCARRIL READING");
         tablero[6] = propiedades[2];
-        tablero[7] = new Fortuna(7);
+        tablero[7] = new TomarCarta(7, "FORTUNA", fortuna);
         tablero[8] = propiedades[3];
         tablero[9] = propiedades[4];
-        tablero[10] = new Carcel(10);
+        tablero[10] = deVisita;
         tablero[11] = propiedades[5];
         tablero[12] = new Sevicio(12, "COMPAÑÍA DE ELECTRICIDAD");
         tablero[13] = propiedades[6];
         tablero[14] = propiedades[7];
         tablero[15] = new Ferrocaril(15, "FERROCARRIL PENSYLVANIA");
         tablero[16] = propiedades[8];
-        tablero[17] = new ArcaComunal(7);
+        tablero[17] = new TomarCarta(17, "ARCA COMUNAL", arcaComunal);
         tablero[18] = propiedades[9];
         tablero[19] = propiedades[10];
-        tablero[20] = new Libre(20);
+        tablero[20] = paradaLibre;
         tablero[21] = propiedades[11];
-        tablero[22] = new Fortuna(22);
+        tablero[22] = new TomarCarta(22, "FORTUNA", fortuna);
         tablero[23] = propiedades[12];
-        tablero[23] = propiedades[13];
-        tablero[24] = new Ferrocaril(24, "FERROCARRIL B. & O.");
-        tablero[25] = propiedades[14];
-        tablero[26] = propiedades[15];
-        tablero[27] = new Sevicio(12, "COMPAÑÍA DE AGUA");
-        tablero[28] = propiedades[16];
-        tablero[29] = new Carcel(29);
-        tablero[30] = propiedades[17];
-        tablero[31] = propiedades[18];
-        tablero[32] = new ArcaComunal(31);
-        tablero[32] = propiedades[19];
-        tablero[34] = new Ferrocaril(34, "FERROCARRIL VÍA RÁPIDA");
-        tablero[35] = new Fortuna(35);
-        tablero[36] = propiedades[20];
-        tablero[37] = new Impuesto(37, "IMPUESTO SOBRE POSESIONES DE LUJO PÁGUENSE", 100);
-        tablero[38] = propiedades[21];  
+        tablero[24] = propiedades[13];
+        tablero[25] = new Ferrocaril(25, "FERROCARRIL B. & O.");
+        tablero[26] = propiedades[14];
+        tablero[27] = propiedades[15];
+        tablero[28] = new Sevicio(28, "COMPAÑÍA DE AGUA");
+        tablero[29] = propiedades[16];
+        tablero[30] = irCarcel;
+        tablero[31] = propiedades[17];
+        tablero[32] = propiedades[18];
+        tablero[33] = new TomarCarta(33, "ARCA COMUNAL", arcaComunal);
+        tablero[34] = propiedades[19];
+        tablero[35] = new Ferrocaril(35, "FERROCARRIL VÍA RÁPIDA");
+        tablero[36] = new TomarCarta(36, "FORTUNA", fortuna);
+        tablero[37] = propiedades[20];
+        tablero[38] = new Impuesto(38, "IMPUESTO SOBRE POSESIONES DE LUJO PÁGUENSE", 100);
+        tablero[39] = propiedades[21];
+    }
+
+    public static Casilla getActualCasilla(Jugador actualJugador) {
+        int pos = actualJugador.getPosicion();
+        return tablero[pos];
     }
 }
