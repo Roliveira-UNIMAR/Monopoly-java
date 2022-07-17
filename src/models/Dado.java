8c612c02a0971a315f7d8408e9bf7bdc5294255a
Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>
2022-07-17 02:19:26 -0400

    Actulizado Signed-off-by:Rodrigo Oliveira <roliveira.5609@unimar.edu.ve>

--- src/models/Dado.java
+++ src/models/Dado.java
@@ -1,23 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.util.Random;
 
 /**
  *
+ * @author Edgar Ruiz - 30.065.695
  * @author Rodrigo Oliveira - 29.655.609
  */
 public class Dado {
     static Random random = new Random();
     public static int dado1, dado2;
     
-    public static void lanzarDado(){
+    public static int lanzar() {
         dado1 = random.nextInt(6) + 1;
-        dado2 = random.nextInt(6) + 1; 
+        dado2 = random.nextInt(6) + 1;
+        return dado1 + dado2;
     }
     
     public static boolean isDoble() {
